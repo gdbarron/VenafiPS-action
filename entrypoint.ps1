aa1900ed8551bdf99e291f9dedf2a4b4bc2676c1
Greg Brownstein <greg@jagtechnical.com>
2022-04-29 19:58:37 -0400

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,7 +1,21 @@
+$server = $env:INPUT_SERVER
+$token = $env:INPUT_TOKEN
 
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module VenafiPS -ErrorAction Stop
 
-New-VenafiSession -Server ($env:INPUT_SERVER) -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($env:INPUT_TOKEN | ConvertTo-SecureString -AsPlainText -Force)))
+$objectGuid = [System.Guid]::empty
+
+if ( [System.Guid]::TryParse($token, [System.Management.Automation.PSReference]$objectGuid) ) {
+
+  Write-Output 'Token is VaaS key'
+  New-VenafiSession -VaasKey (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
+}
+else {
+
+  Write-Output 'Token is TPP token'
+  New-VenafiSession -Server $server -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
+}
+
 
 Invoke-Expression $env:INPUT_COMMANDS
