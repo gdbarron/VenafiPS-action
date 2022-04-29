de9f98c4e7b854222b7290a6237df8296d6b787a
Greg Brownstein <greg@jagtechnical.com>
2022-04-29 19:45:14 -0400

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -3,6 +3,6 @@ gci env:
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module VenafiPS -ErrorAction Stop
 
-New-VenafiSession -Server ($env:input_server) -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($env:input_token | ConvertTo-SecureString -AsPlainText -Force)))
+New-VenafiSession -Server ($env:INPUT_SERVER) -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($env:INPUT_TOKEN | ConvertTo-SecureString -AsPlainText -Force)))
 
-Invoke-Expression $env:input_commands
+Invoke-Expression $env:INPUT_COMMANDS
