861be0ed344a332436e5c13ba2f3b09c31d54412
Greg Brownstein <greg@jagtechnical.com>
2022-06-27 12:44:28 -0400

    get args

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,33 +1,37 @@
-$server = $env:INPUT_TPP_SERVER
-$token = $env:INPUT_TOKEN
+# $server = $env:INPUT_TPP_SERVER
+# $token = $env:INPUT_TOKEN
 
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module VenafiPS -ErrorAction Stop
+# Set-PSRepository PSGallery -InstallationPolicy Trusted
+# Install-Module VenafiPS -ErrorAction Stop
 
-$objectGuid = [System.Guid]::empty
+# $objectGuid = [System.Guid]::empty
 
-if ( -not $token ) {
-  throw "'token' parameter not provided"
-}
+# if ( -not $token ) {
+#   throw "'token' parameter not provided"
+# }
 
-if ( [System.Guid]::TryParse($token, [System.Management.Automation.PSReference]$objectGuid) ) {
+# if ( [System.Guid]::TryParse($token, [System.Management.Automation.PSReference]$objectGuid) ) {
 
-  Write-Output 'Token is VaaS key'
-  New-VenafiSession -VaasKey (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
-}
-else {
+#   Write-Output 'Token is VaaS key'
+#   New-VenafiSession -VaasKey (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
+# }
+# else {
 
-  Write-Output 'Token is TPP token'
+#   Write-Output 'Token is TPP token'
 
-  if ( -not $server ) {
-    throw "'server' parameter is required when connecting to TPP"
-  }
+#   if ( -not $server ) {
+#     throw "'server' parameter is required when connecting to TPP"
+#   }
 
-  New-VenafiSession -Server $server -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
-}
+#   New-VenafiSession -Server $server -AccessToken (New-Object System.Management.Automation.PSCredential('AccessToken', ($token | ConvertTo-SecureString -AsPlainText -Force)))
+# }
 
 
-Invoke-Expression $env:INPUT_COMMANDS
+# Invoke-Expression $env:INPUT_COMMANDS
+
+$env:TPP_SERVER = $args[0]
+$env:TPP_TOKEN = $args[1]
+$env:VAAS_KEY = $args[2]
 
 if ( $action_out ) {
   Write-Output "::set-output name=action_out::$action_out"
